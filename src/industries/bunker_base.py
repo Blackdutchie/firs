1fa1b692a2a61b72d22df6e09f838741c26077b0
Blackdutchie <deathbydorfattack@gmail.com>
2025-12-06 13:02:57 +0100

    BB graphics try 1

--- src/industries/bunker_base.py
+++ src/industries/bunker_base.py
@@ -4,8 +4,8 @@ industry = IndustryTertiary(
     id="bunker_base",
     accept_cargo_types=["FRNT"],
     prod_cargo_types=[],
-    prob_in_game="18",
-    prob_map_gen="24",
+    prob_in_game="0",
+    prob_map_gen="0",
     prod_multiplier="[0, 0]",
     map_colour="169",
     colour_scheme_name="scheme_3_hendrix",
@@ -15,7 +15,7 @@ industry = IndustryTertiary(
     name="string(STR_IND_BUNKER_BASE)",
     nearby_station_name="string(STR_STATION_TOWN_1)",
     fund_cost_multiplier="15",
-    provides_snow=True,
+    provides_snow=False,
     sprites_complete=True,
     animated_tiles_fixed=True,
 )
@@ -25,20 +25,45 @@ industry.enable_in_economy(
 )
 
 spriteset_ground = industry.add_spriteset(
-    type="pavement",
+    type="gravel",
 )
-spriteset_ground_overlay = industry.add_spriteset(
-    sprites=[(10, 10, 64, 31, -31, 0)],
+
+industry.add_tile(
+    id="bunker_base_tile_1",
+    location_checks=TileLocationChecks(
+        require_effectively_flat=True, disallow_industry_adjacent=True
+    ),
 )
-spriteset_1 = industry.add_spriteset(sprites=[(10, 60, 64, 48, -31, -18)])
+spriteset_1 = industry.add_spriteset(
+    sprites=[(150, 10, 64, 64, -31, -31)],
+)
+
+spriteset_2 = industry.add_spriteset(
+    sprites=[(220, 10, 64, 92, -31, -60)],
+)
+
 industry.add_spritelayout(
-    id="bunker_base_spritelayout",
-    tile="general_store_tile_1",
+    id="bunker_base_spritelayout_1",
+    tile="bunker_base_tile_1",
     ground_sprite=spriteset_ground,
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[spriteset_1],
 )
+industry.add_spritelayout(
+    id="bunker_base_spritelayout_2",
+    tile="bunker_base_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[spriteset_2],
+)
 industry.add_industry_layout(
-    id="bunker_base_industry_layout",
-    layout=[(0, 0, "bunker_base_spritelayout")],
+    id="bunker_base_industry_layout_1",
+    layout=[
+        (0, 0, "bunker_base_spritelayout_1"),
+        (0, 1, "bunker_base_spritelayout_1"),
+        (1, 0, "bunker_base_spritelayout_1"),
+        (1, 1, "bunker_base_spritelayout_1"),
+        (2, 0, "bunker_base_spritelayout_2"),
+        (2, 1, "bunker_base_spritelayout_1"),
+    ],
 )
