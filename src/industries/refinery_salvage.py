3406ec0d1a6121bc36f328d6c903be10e3388e77
Blackdutchie <deathbydorfattack@gmail.com>
2025-12-09 01:28:11 +0100

    Update refinery_salvage.py

--- src/industries/refinery_salvage.py
+++ src/industries/refinery_salvage.py
@@ -51,10 +51,10 @@ spriteset_3 = industry.add_spriteset(
     sprites=[(720, 110, 64, 84, -31, -54)],
 )
 spriteset_4 = industry.add_spriteset(
-    sprites=[(440, 410, 64, 84, -31, 0)],
+    sprites=[(440, 410, 64, 84, -31, -54)],
 )
 spriteset_5 = industry.add_spriteset(
-    sprites=[(251, 505, 64, 31, -31, -31)],
+    sprites=[(251, 505, 64, 31, -31, -0)],
 )
 
 industry.add_spritelayout(
